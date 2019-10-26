a467bf9a7baeef4daf479ac7c459f603a2d74989
Mikołaj Rosowski <m.rosowski1@wp.pl>
2019-10-26 18:03:41 +0200

    Created course model

--- models/bootcampModel.js
+++ models/bootcampModel.js
@@ -5,16 +5,16 @@ const geocoder = require('../utils/geocoder');
 const bootcampSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please add a name.'],
+    required: [true, 'Please add a name'],
     unique: true,
     trim: true,
-    maxlength: [50, 'Name cannot exceed 50 characters.']
+    maxlength: [50, 'Name cannot exceed 50 characters']
   },
   slug: String,
   description: {
     type: String,
-    required: [true, 'Please add a description.'],
-    maxlength: [250, 'Description cannot exceed 250 characters.']
+    required: [true, 'Please add a description'],
+    maxlength: [250, 'Description cannot exceed 250 characters']
   },
   website: {
     type: String,
@@ -25,18 +25,18 @@ const bootcampSchema = new mongoose.Schema({
   },
   phone: {
     type: String,
-    maxlength: [20, 'Phone number cannot exceed 20 characters.']
+    maxlength: [20, 'Phone number cannot exceed 20 characters']
   },
   email: {
     type: String,
     match: [
       /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
-      'Please enter a valid email address.'
+      'Please enter a valid email address'
     ]
   },
   address: {
     type: String,
-    required: [true, 'Please add an address.']
+    required: [true, 'Please add an address']
   },
   location: {
     type: {
---
+++ models/courseModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add a title']
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add a description'],
+    maxlength: [250, 'Description cannot exceed 250 characters']
+  },
+  weeks: {
+    type: String,
+    required: [true, 'Please add a number of weeks']
+  },
+  tuition: {
+    type: Number,
+    required: [true, 'Please add a tuition'],
+    min: [0, 'Tuition cannot be less than 0']
+  },
+  minimumSkill: {
+    type: String,
+    required: [true, 'Please add a minimum skill'],
+    enum: ['beginner', 'intermediate', 'advanced']
+  },
+  scholarshipAvailable: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  bootcamp: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Bootcamp',
+    required: true
+  }
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+module.exports = Course;
