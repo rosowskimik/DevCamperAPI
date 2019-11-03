e9ae1db8f8845df60bde889fca49b2fba79b905f
Mikołaj Rosowski <m.rosowski1@wp.pl>
2019-11-03 12:05:20 +0100

    Created Review model

---
+++ models/reviewModel.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    maxlength: 100,
+    required: [true, 'Please add a title']
+  },
+  text: {
+    type: String,
+    required: [true, 'Please add some text']
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10,
+    required: [true, 'Please add a rating between 1 and 10']
+  },
+  bootcamp: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Bootcamp',
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
+
+// Static for calculating bootcamp's average rating
+courseSchema.statics.getAverageRating = async function(bootcampId) {
+  const [obj] = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageRating: obj.averageRating.toFixed(2) * 1
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+reviewSchema.pre('remove', async function(next) {
+  await this.constructor.getAverageRating(this.bootcamp);
+  next();
+});
+reviewSchema.post('save', async function() {
+  await this.constructor.getAverageRating(this.bootcamp);
+});
+reviewSchema.post('findOneAndUpdate', async function(doc) {
+  await this.model.getAverageRating(doc.bootcamp);
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
